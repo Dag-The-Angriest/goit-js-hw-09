3b7738ac3e4db5350d3a5a51bc1aff6304a2a3a0
Roman Kovelskyi <kovelsky.roman@gmail.com>
2026-03-03 19:42:59 +0100

    do task 2

---
+++ src/js/1-gallery.js
@@ -0,0 +1,7 @@
+import SimpleLightbox from 'simplelightbox';
+
+SimpleLightbox.open({
+  content:
+    '<div class="contentInPopup"><h3 class="attireTitleType3">Custom content</h3>...',
+  elementClass: 'slbContentEl',
+});
---
+++ src/js/2-form.js
@@ -0,0 +1,55 @@
+const form = document.querySelector('.feedback-form');
+
+document.addEventListener('DOMContentLoaded', e => {
+  //   loadFromLS(formData.email);
+  const loaded = loadFromLS('formData');
+  console.log(loaded);
+  formData.email = loaded.email;
+  formData.message = loaded.message;
+  form.elements.email.value = formData.email;
+  form.elements.message.value = formData.message;
+  console.log(formData);
+});
+
+const formData = {
+  email: '',
+  message: '',
+};
+// const formData = new FormData();
+// formData.append('email');
+// formData.append('message');
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  if (
+    form.elements.email.value.trim() === '' ||
+    form.elements.message.value.trim() === ''
+  ) {
+    return alert('Fill please all fields');
+  }
+  form.reset();
+});
+form.addEventListener('input', e => {
+  e.preventDefault();
+
+  const formInput = new FormData(form);
+  const email = formInput.get('email');
+  const message = formInput.get('message');
+  formData.email = email.trim();
+  formData.message = message.trim();
+  saveToLS('formData', formData);
+  console.log(formData);
+});
+
+function saveToLS(key, value) {
+  const json = JSON.stringify(value);
+  localStorage.setItem(key, json);
+}
+function loadFromLS(key, defaultValue) {
+  const jsonData = localStorage.getItem(key);
+  try {
+    const data = JSON.parse(jsonData);
+    return data ?? defaultValue;
+  } catch {
+    return jsonData ?? defaultValue;
+  }
+}
